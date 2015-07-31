0d18b8d6cab39d9e77a276d947d4c3b133730b33
LukeJS <lukejsandler@gmail.com>
2015-07-31 18:21:24 +0100

    Refactoring, registering items properly, crafting, trying to get resources to work, WIP

---
+++ src/main/java/me/nentify/drillify/item/DrillifyItems.java
@@ -0,0 +1,32 @@
+package me.nentify.drillify.item;
+
+import cofh.lib.util.helpers.EnergyHelper;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import static cofh.lib.util.helpers.ItemHelper.ShapedRecipe;
+
+public class DrillifyItems {
+
+    public static void preInit() {
+        itemDrill = new ItemDrill();
+
+        GameRegistry.registerItem(itemDrill, "drill");
+    }
+
+    public static void init() {
+        toolDrill = EnergyHelper.setDefaultEnergyTag(new ItemStack(itemDrill, 1, 0), 0);
+
+        GameRegistry.registerCustomItemStack("toolDrill", toolDrill);
+    }
+
+    public static void postInit() {
+        GameRegistry.addRecipe(ShapedRecipe(toolDrill, new Object[] { "   ", " I ", "   ", 'I', "ingotIron" }));
+    }
+
+    public static Item itemDrill;
+
+    public static ItemStack toolDrill;
+}
--- src/main/java/me/nentify/drillify/item/ItemDrill.java
+++ src/main/java/me/nentify/drillify/item/ItemDrill.java
@@ -1,8 +1,9 @@
-package me.nentify.drillify;
+package me.nentify.drillify.item;
 
 import cofh.api.energy.IEnergyContainerItem;
 import cofh.lib.util.helpers.EnergyHelper;
 import cofh.lib.util.helpers.StringHelper;
+import me.nentify.drillify.Drillify;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
@@ -20,11 +21,10 @@ public class ItemDrill extends ItemPickaxe implements IEnergyContainerItem {
     public int maxTransfer = 1000;
     public int energyPerUse = 500;
 
-    int damage = 0;
-
     public ItemDrill() {
         super(Item.ToolMaterial.IRON);
-        setUnlocalizedName("ItemDrill");
+        setUnlocalizedName(Drillify.PREFIX + "drill");
+        setTextureName(Drillify.RESOURCE_PREFIX + "drill");
         setMaxDamage(100);
         setNoRepair();
     }
@@ -37,13 +37,18 @@ public class ItemDrill extends ItemPickaxe implements IEnergyContainerItem {
     }
 
     @Override
+    public void getSubItems(Item item, CreativeTabs tab, List list) {
+        list.add(EnergyHelper.setDefaultEnergyTag(new ItemStack(item, 1, 0), 0));
+        list.add(EnergyHelper.setDefaultEnergyTag(new ItemStack(item, 1, 0), maxEnergy));
+    }
+
+    @Override
     public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean check) {
         if (stack.stackTagCompound == null) {
             EnergyHelper.setDefaultEnergyTag(stack, 0);
         }
 
-        list.add(StringHelper.ORANGE + "Energy: " + stack.stackTagCompound.getInteger("energy") + " / " + maxEnergy + " RF");
-        list.add("Damage: " + stack.getItemDamage());
+        list.add(StringHelper.ORANGE + "Energy: " + stack.stackTagCompound.getInteger("Energy") + " / " + maxEnergy + " RF");
     }
 
     @Override
@@ -54,7 +59,7 @@ public class ItemDrill extends ItemPickaxe implements IEnergyContainerItem {
 
     @Override
     public float getDigSpeed(ItemStack stack, Block block, int meta) {
-        if (stack.stackTagCompound.getInteger("energy") < energyPerUse) {
+        if (stack.stackTagCompound.getInteger("Energy") < energyPerUse) {
             return 0.1F;
         } else {
             return ToolMaterial.EMERALD.getEfficiencyOnProperMaterial();
@@ -73,12 +78,12 @@ public class ItemDrill extends ItemPickaxe implements IEnergyContainerItem {
             EnergyHelper.setDefaultEnergyTag(container, 0);
         }
 
-        int energy = container.stackTagCompound.getInteger("energy");
+        int energy = container.stackTagCompound.getInteger("Energy");
         int energyReceived = Math.min(maxReceive, Math.min(maxEnergy - energy, maxTransfer));
 
         if (!simulate) {
             energy += energyReceived;
-            container.stackTagCompound.setInteger("energy", energy);
+            container.stackTagCompound.setInteger("Energy", energy);
             container.setItemDamage(energyToPercentage(energy));
         }
 
@@ -91,12 +96,12 @@ public class ItemDrill extends ItemPickaxe implements IEnergyContainerItem {
             EnergyHelper.setDefaultEnergyTag(container, 0);
         }
 
-        int energy = container.stackTagCompound.getInteger("energy");
+        int energy = container.stackTagCompound.getInteger("Energy");
         int energyExtracted = Math.min(maxExtract, energy);
 
         if (!simulate) {
             energy -= energyExtracted;
-            container.stackTagCompound.setInteger("energy", energy);
+            container.stackTagCompound.setInteger("Energy", energy);
             container.setItemDamage(energyToPercentage(energy));
         }
 
@@ -109,7 +114,7 @@ public class ItemDrill extends ItemPickaxe implements IEnergyContainerItem {
             EnergyHelper.setDefaultEnergyTag(container, 0);
         }
 
-        return container.stackTagCompound.getInteger("energy");
+        return container.stackTagCompound.getInteger("Energy");
     }
 
     @Override
