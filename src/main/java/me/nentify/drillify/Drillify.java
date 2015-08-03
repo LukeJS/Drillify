0a47c34b114d0b24a632824ead709b553cb41e40
LukeJS <lukejsandler@gmail.com>
2015-08-04 00:05:22 +0100

    Slight build modifications and added license

--- src/main/java/me/nentify/drillify/Drillify.java
+++ src/main/java/me/nentify/drillify/Drillify.java
@@ -13,7 +13,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = Drillify.MODID, version = Drillify.VERSION, dependencies = Drillify.DEPENDENCIES)
 public class Drillify {
     public static final String MODID = "drillify";
-    public static final String VERSION = "0.2";
+    public static final String VERSION = "@VERSION@";
     public static final String PREFIX = MODID + ".";
     public static final String RESOURCE_PREFIX = MODID + ":";
     public static final String DEPENDENCIES = "required-after:Forge@[10.13.2.1291,);after:ThermalExpansion";
