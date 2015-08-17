a6dc55e3c067927d99a04ec248a23ee9e1daca87
LukeJS <lukejsandler@gmail.com>
2015-08-17 13:59:57 +0100

    Drill no longer swings if it has charge

--- src/main/java/me/nentify/drillify/item/drill/ItemDrill.java
+++ src/main/java/me/nentify/drillify/item/drill/ItemDrill.java
@@ -11,6 +11,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
@@ -78,6 +79,19 @@ public class ItemDrill extends ItemToolAdv implements IEnergyContainerItem, IEqu
     @Override
     public boolean showDurabilityBar(ItemStack stack) {
         return true;
+    }
+
+    @Override
+    public boolean onEntitySwing(EntityLivingBase entity, ItemStack stack) {
+        if (stack.stackTagCompound == null) {
+            EnergyHelper.setDefaultEnergyTag(stack, 0);
+        }
+
+        if (stack.stackTagCompound.getInteger("Energy") > 0) {
+            return true;
+        }
+
+        return false;
     }
 
     @Override
